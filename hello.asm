efad2f30b50a249fd88c94a36dbef0003f186c01
Alê <89224363+helloAle@users.noreply.github.com>
2023-04-09 15:37:52 -0300

    Update hello.asm

--- hello.asm
+++ hello.asm
@@ -15,8 +15,11 @@ global _start
 _start>
     mov EAX, 0x4
     mov EBX, 0x1
+    mov ECX, msg
+    mov EDX, tam
+    int 0x80
 
     mov EAX, 0x1 ; SO estou terminando o programa
     mov EBX, 0x0 ; SO o valor de retorno é 0
-    int 0x89
+    int 0x80
 
